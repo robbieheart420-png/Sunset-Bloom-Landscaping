65f2f8980c0a5baac181203e9c1ef55e12cffe8a
robbieheart420-png <robbieheart420@gmail.com>
2026-06-30 01:44:12 -0700

    Change hero image to background image on click
    Updated the hero image handling to use background image instead of src.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,10 @@
-const heroImage = document.querySelector('.hero-image');
+const hero = document.querySelector('.hero');
 const links = document.querySelectorAll('.top-nav a');
 
 links.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     const newImage = e.target.getAttribute('data-image');
-    heroImage.classList.add('fade-out');
-    setTimeout(() => {
-      heroImage.src = `assets/images/${newImage}`;
-      heroImage.classList.remove('fade-out');
-      heroImage.classList.add('fade-in');
-      setTimeout(() => heroImage.classList.remove('fade-in'), 800);
-    }, 400);
+    hero.style.backgroundImage = `url('${newImage}')`;
   });
 });
